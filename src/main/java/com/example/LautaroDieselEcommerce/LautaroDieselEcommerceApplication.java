4478af8f1d554f3210545ece336efe4cc1c36827
Manuel Artusa <114393@tecnicatura.frc.utn.edu.ar>
2025-09-28 12:19:24 -0300

    Estructura inicial

---
+++ src/main/java/com/example/LautaroDieselEcommerce/LautaroDieselEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.LautaroDieselEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LautaroDieselEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LautaroDieselEcommerceApplication.class, args);
+	}
+
+}

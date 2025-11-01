962b72b2389922b7ba06b0909a34ac14b79f7842
Manuel Artusa <114393@tecnicatura.frc.utn.edu.ar>
2025-11-01 11:49:06 -0300

    Update app.routes.ts

--- FRONTEND/src/app/app.routes.ts
+++ FRONTEND/src/app/app.routes.ts
@@ -5,13 +5,16 @@ import { Register } from './register/register';
 import { Profile } from './profile/profile';
 import { AuthGuard } from './services/auth.guard';
 import { PanelAdmin } from './panel-admin/panel-admin';
+import { PasswordRecovery } from './password-recovery/password-recovery';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'login', component: Login },
+  { path: 'password-recovery', component: PasswordRecovery },
   { path: 'register', component: Register },
   { path: 'profile', component: Profile, canActivate: [AuthGuard] },
   { path: 'panel-admin', component: PanelAdmin, canActivate: [AuthGuard] },
+
 ];
